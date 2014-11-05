77bd90e6b80f3e411d1145ff17c048bf4548e378
hongfei <hongfei1991@hotmail.com>
2014-11-05 15:32:57 +0800

    添加关于和评论

--- Eddyt.Blog.Web/Controllers/HomeController.cs
+++ Eddyt.Blog.Web/Controllers/HomeController.cs
@@ -5,12 +5,14 @@ using System.Web;
 using System.Web.Mvc;
 
 using Eddyt.Blog.Business;
+using Eddyt.Blog.Data;
 
 namespace Eddyt.Blog.Web.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ArticleManager articleManager=new ArticleManager();
+        private readonly CommentManager commentManager = new CommentManager();
 
         public ActionResult Index(int page=1)
         {
@@ -19,21 +21,32 @@ namespace Eddyt.Blog.Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "About";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
+            return View(commentManager.GetAboutComments());
         }
 
         public ActionResult GuestBook()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult AddComment(Comment comment)
+        {
+            comment.CommentId = Guid.NewGuid().ToString();
+            comment.CreateTime = DateTime.UtcNow;
+
+            try
+            {
+                commentManager.AddComment(comment);
+                return Redirect("~/Home/About");
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
         }
     }
 }
